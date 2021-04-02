09c6124a3b77b50d095b6d3894864b4117eaa742
Joshua Kurdys <jkurdys@gmail.com>
2021-04-02 08:22:52 -0600

    added docstrings to log_reg_model.py

--- src/log_reg_model.py
+++ src/log_reg_model.py
@@ -7,11 +7,27 @@ import statsmodels.formula.api as smf
 from data_pipeline import clean_data
 
 def split_X_y(df, target_col):
+    '''
+    splits clean df into features, X, and targets, y,
+    using specified target column to select targets.
+
+    inputs: clean df, target_col from clean df 
+    returns: features (X), targets (y)
+    '''
     X = df.copy()
     y = X.pop(target_col)
     return X, y
 
 def confirm_assumptions(X,y):
+    '''
+    confirms whether sets of independent variables (X)
+    and dependent variables (y) meet assumptions of 
+    logistic regression.
+    inputs: independent variables (X), dependent variables (y)
+    returns: prints boolean confirmation of binary dependent
+    variable, non-collinearity of independent variables and
+    sample-size assumptions of logistic regression.
+    '''
     binary_dv = y.nunique() == 2
     corr_mat = X.corr()[X.corr() < 1]
     no_corr = corr_mat.describe()[-1:] >= .4
@@ -24,10 +40,20 @@ def confirm_assumptions(X,y):
 
 def find_coefs(X, y, i):
     '''
-    Select index, i, of grouping on which to run regression summary.
+    evaluates logistic regression model on various feature 
+    configurations for coefficient values and scoring metrics
+    inputs: features (X), targets (y), index value for formula
+    used for regression (i =
+                            0: trivial features of voter status and age
+                            1: racial demographic features
+                            2: gender demographic features
+                            3: county composition features
+                            4: congressional district geographic features
+                            5: complete feature set)
+    returns: summary statistics of model
     '''
     
-    groupings = ['y~ C(voter_status) + C(rural) + age',
+    groupings = ['y~ C(voter_status) + age',
                 'y~ C(AI) + C(AP) + C(BH) + C(HP) + C(OT) + C(U) + C(WH)',
                 'y~ C(F) + C(M) + C(O)',
                 'y~ C(rural) + C(urban) + C(military)',
@@ -37,6 +63,11 @@ def find_coefs(X, y, i):
     return model.summary()
 
 def lr_model(X, y):
+    '''
+    preliminary setup for sk-learn logistic regression model
+    inputs: features (X), targets (y)
+    returns: train test splits for features and targets
+    '''
     X_train, X_test, y_train, y_test = train_test_split(X, y)
     regressor = LogisticRegression(class_weight = 'balanced',
                                    solver='liblinear')
